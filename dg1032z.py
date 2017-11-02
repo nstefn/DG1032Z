dcda01a6b3dbaa8d083f1f007a8d47ded8e21ace
Nemanja Stefanovic <nemanja.stef92@gmail.com>
2017-11-02 17:55:38 +0100

    added a few basic functionalities

--- dg1032z.py
+++ dg1032z.py
@@ -17,17 +17,34 @@ class DG1032Z(vxi11.Instrument):
         super(DG1032Z, self).__init__(self.host)
 
     
-    def write_string(self, cmd):
+    def __write_string(self, cmd):
         super(DG1032Z, self).write(cmd)
 
     
-    def gen_signal(self, output, signal, freq, ampl, offset, phase):
-        self.write_string(':{}:{}:{} {},{},{}'.format(output, signal, freq, ampl, \
+    def generate_signal(self, output, signal, freq, ampl, offset, phase):
+        self.__write_string(':{}:APPL:{} {},{},{},{}'.format(output, signal, freq, ampl, \
                 offset, phase))
     
+    def set_voltage_unit(self, output, unit):
+        self.__write_string(':SOUR{}:VOLT:UNIT {}'.format(output, unit))
+
+
+    def start_output(self, output):
+        if output == 1:
+            self.__write_string(':OUTP1 ON')
+        else:
+            self.__write_string(':OUTP2 ON')
+
+
+    def stop_output(self, output):
+        if output == 1:
+            self.__write_string(':OUTP1 OFF')
+        else:
+            self.__write_string(':OUTP2 OFF')
+
     
     def get_dev_id(self):
-        self.write_string('*IDN?')
+        self.__write_string('*IDN?')
 
     
     def check_conn(self):
